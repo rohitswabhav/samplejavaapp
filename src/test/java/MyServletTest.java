aee0c64ae0a3b23791e28cbf31451e292b10f7f2
Rohit Devikar <rohit@swabhavtechlabs.com>
2025-10-26 23:47:20 +0530

    Create MyServletTest.java

---
+++ src/test/java/MyServletTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyServletTest {
+
+    @Test
+    public void sampleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
